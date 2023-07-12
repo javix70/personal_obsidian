54d9f7d39426ca1491c8048bf95aec5fce9bce17
Javier pacheco <javier.pacheco@niufoods.cl>
2023-07-11 21:06:19 -0400

    vault backup: 2023-07-11 21:06:19

--- 201_scripts/reorganize_checkboxes.js
+++ 201_scripts/reorganize_checkboxes.js
@@ -6,24 +6,15 @@ function reorganize(tp){
   const filesIntoFolder = fs.readdirSync(currentFolderPath)
   
   const currentFileName = tp.config.active_file.basename
-  
-  debugger
+
   let dateObjects = filesIntoFolder.map(filename => new Date((filename.replace('.md', '') + 'T00:00:00').split('_')[0]));
   dateObjects = dateObjects.filter(date => date.toISOString().split('T')[0] !== currentFileName);
-  debugger
+
   dateObjects.sort((a, b) => b - a);
-  let highestDate = dateObjects[0];
+  let highestDate = dateObjects[0].toISOString();
 
-  // TODO: pensar que hacer cuando hay dias faltantes entremedio
-  const previousDate = new Date(nameToDate.getTime());
-  previous.setDate(date.getDate() - 1);
-
+  const previousFileName = filesIntoFolder.find(filename => filename.includes(highestDate));
   debugger
-  const previousFileName = filesIntoFolder.find(filename => {
-    const match = filename.match(/^(\d+)_/)
-    return match ? +match[1] === previousDate : false
-  });
-
   const previousFile = tp.file.find_tfile(previousFileName)
   
   debugger
